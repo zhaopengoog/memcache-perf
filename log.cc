5e195b300db895550b2bc1a143c21e3210ddddc6
Shay Gal-On <sgalon@cavium.com>
2016-11-16 09:31:34 -0800

    optimize log

--- log.cc
+++ log.cc
@@ -7,13 +7,15 @@ log_level_t log_level = INFO;
 
 void log_file_line(log_level_t level, const char *file, int line,
                    const char *format, ...) {
-  va_list args;
-  char new_format[512];
+  
+  if (level >= log_level) {
+    va_list args;
+    char new_format[512];
 
-  snprintf(new_format, sizeof(new_format), "%s(%d): %s\n", file, line, format);
+    snprintf(new_format, sizeof(new_format), "%s(%d): %s\n", file, line, format);
 
-  va_start(args, format);
-  if (level >= log_level)
+    va_start(args, format);
     vfprintf(stderr, new_format, args);
-  va_end(args);
+    va_end(args);
+  }
 }
